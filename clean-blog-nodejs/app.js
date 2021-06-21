91b09f55bad5e8ab0d5128d93979c37f23923a0c
aberimen <berimen63@gmail.com>
2021-06-21 13:18:50 +0300

    Add ejb

--- clean-blog-nodejs/app.js
+++ clean-blog-nodejs/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
 
+//template engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'template/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add-new-post', (req, res) => {
+  res.render('add_post');
 });
 
 app.listen(port, () => {
-  console.log('app started on' + port);
+  console.log('app started on: ' + port);
 });
