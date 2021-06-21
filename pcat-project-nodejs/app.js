260f3aeb2222b53e21b406525e822f83dbf06a11
aberimen <berimen63@gmail.com>
2021-06-21 10:23:16 +0300

    Add express and nodemon

--- pcat-project-nodejs/app.js
+++ pcat-project-nodejs/app.js
@@ -1,1 +1,17 @@
-console.log('saadasd');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    title: 'title',
+    description: 'description',
+  };
+
+  res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log('App started on ' + port);
+});
