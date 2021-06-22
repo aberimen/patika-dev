dd21817721f94d95a0456b5dae4799c90106a41a
aberimen <berimen63@gmail.com>
2021-06-22 18:12:16 +0300

    Send Post data to MongoDB

--- clean-blog-nodejs/app.js
+++ clean-blog-nodejs/app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
 
 const app = express();
 const port = 3001;
 
-app.use(express.static('public'));
+//db
+mongoose.connect('mongodb://localhost/clean-blog', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 //template engine
 app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -22,13 +33,9 @@ app.get('/add-new-post', (req, res) => {
   res.render('add_post');
 });
 
-const jobs = [
-  { id: 1, name: 'Node.js Developer' },
-  { id: 2, name: 'Node.js Tester' },
-];
-
-app.get('/jobs', (req, res) => {
-  res.send(jobs);
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
